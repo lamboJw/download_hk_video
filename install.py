892d65b5657176e02a4aab2c1e2c1ddfb0479a28
lamboJw <454401890@qq.com>
2022-09-16 11:45:06 +0800

    打包脚本

--- install.py
+++ install.py
@@ -1,9 +1,43 @@
 # -*- encoding=utf-8 -*-
 import os
+import sys
+import shutil
+
 from base_path import get_base_path
 
+BASE_PATH = get_base_path()
+
+
+def remove_dir(path):
+    if not os.path.exists(path):
+        return
+    for file in os.scandir(path):
+        cur_path = os.path.join(path, file)
+        if os.path.isfile(cur_path):
+            os.remove(cur_path)
+        else:
+            remove_dir(cur_path)
+    os.rmdir(path)
+
+
+def copy_dir(source, target):
+    if sys.platform.startswith('win'):
+        os.system(f"xcopy {source} {target} /I/E/Q")
+    else:
+        os.system(f"cp -R {source} {target}")
+
+
 if __name__ == "__main__":
-    result = os.popen(f"cd {get_base_path()} && pyinstaller -F {os.path.join(get_base_path(), 'main.py')}")
-    last_line = result.readlines()[-1]
-    print(last_line)
-    # TODO 删除build文件夹，判断dist是否存在lib、webDriver、config.json文件，无则复制
+    result = os.popen(f"pyinstaller -F {os.path.join(BASE_PATH, 'main.py')}")
+    last_line = result.readlines()
+    remove_dir(os.path.join(BASE_PATH, "build"))
+    dist_dir = os.path.join(BASE_PATH, "dist")
+    if not os.path.exists(os.path.join(dist_dir, "config.json")):
+        shutil.copy(os.path.join(BASE_PATH, 'config.json'), os.path.join(dist_dir, 'config.json'))
+    if not os.path.exists(os.path.join(dist_dir, "lib")):
+        copy_dir(os.path.join(BASE_PATH, 'lib'), os.path.join(dist_dir, 'lib'))
+    if not os.path.exists(os.path.join(dist_dir, "webDriver")):
+        copy_dir(os.path.join(BASE_PATH, 'webDriver'), os.path.join(dist_dir, 'webDriver'))
+        remove_dir(os.path.join(dist_dir, 'webDriver', '__pycache__'))
+        if os.path.exists(os.path.join(dist_dir, 'webDriver', '__init__.py')):
+            os.remove(os.path.join(dist_dir, 'webDriver', '__init__.py'))
